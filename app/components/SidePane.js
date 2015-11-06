19bcca8344665468ba196943f537a0dfd2d89cd6
jhenderiks <justin@henderiks.ca>
2015-11-06 16:48:52 -0500

    ... Created Tweet component Did some more CSS styling (responsiveness of sidepane, colours, etc.)

--- app/components/SidePane.js
+++ app/components/SidePane.js
@@ -1,10 +1,30 @@
 var React = require('react');
+var Tweet = require('./Tweet');
 
 var SidePane = React.createClass({
   render: function() {
     return (
       <div className={"side-pane " + this.props.className}>
-        <h1>I am a SidePane.</h1>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
       </div>
     )
   }
---
+++ app/components/Tweet.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Navbar = React.createClass({
+  render: function() {
+    return (
+      <div className={"tweet"}>
+        <h1>I am a Tweet.</h1>
+      </div>
+    )
+  }
+});
+
+module.exports = Navbar;
