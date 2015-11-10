4a4cb37200d96b09d80ec930a6a10a63233fc484
spiderbites <zuckerje@gmail.com>
2015-11-10 11:16:55 -0500

    handlesubmit function for username search, to be implemented with callback

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,22 @@
 var React = require('react');
 
 var Header = React.createClass({
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var username = this.refs.username.value.trim();
+    if (!username)
+      return;
+    //this.props.onUsernameSubmit({username: username});
+    this.refs.username.value = '';
+  },
+
   render: function() {
     return (
       <div className={"header"}>
-        <input type="search" placeholder="Twitter username"/>
+        <form onSubmit={this.handleSubmit}>
+          <input type="search" placeholder="Twitter username" ref="username" />
+        </form>
       </div>
     )
   }
