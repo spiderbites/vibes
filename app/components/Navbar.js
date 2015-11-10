b8350fdc2b8aba65dc316fb3ced4a6e68bc247bf
jhenderiks <justin@henderiks.ca>
2015-11-10 12:12:53 -0500

    Navbar click-to-switch-content functionality added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,12 +1,16 @@
 var React = require('react');
 
 var Navbar = React.createClass({
+  selectContent: function (event) {
+    this.props.selectContent(event.target.textContent);
+  },
+
   render: function() {
     return (
       <div className={"navbar"}>
         <ul>
-          <li>Chart</li>
-          <li>Map</li>
+          <li onClick={this.selectContent}><span>Chart</span></li>
+          <li onClick={this.selectContent}><span>Map</span></li>
         </ul>
       </div>
     )
