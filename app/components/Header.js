f251404cc9d75f493308414ddde324a4db59bb10
spiderbites <zuckerje@gmail.com>
2015-11-11 16:23:10 -0500

    added an hours input field

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,20 +2,26 @@ var React = require('react');
 
 var Header = React.createClass({
 
+  placeholder: {search: "@handle, #hash, etc...", number: "Hours"},
+
   handleSubmit: function(e) {
     e.preventDefault();
-    var username = this.refs.username.value.trim();
-    if (!username)
+    var query = this.refs.query.value.trim();
+    var hours = this.refs.hours.value;
+    if (!query)
       return;
-    this.props.onUsernameSubmit({q: username});
-    this.refs.username.value = '';
+    this.props.onQuerySubmit({q: query, hours: hours});
+    this.refs.query.value = "";
+    this.refs.hours.value = this.placeholder.number;
   },
 
   render: function() {
     return (
       <div className={"header"}>
         <form onSubmit={this.handleSubmit}>
-          <input type="search" placeholder="Twitter username" ref="username" />
+          <input type="search" placeholder={this.placeholder.search} ref="query" />
+          <input type="number" min="1" max="24" ref="hours" placeholder={this.placeholder.number}/>
+          <input type="submit" />
         </form>
       </div>
     )
