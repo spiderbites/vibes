f4ea516338e39965e66461098e7fa9b59f912073
spiderbites <zuckerje@gmail.com>
2015-11-12 12:05:19 -0500

    added days field, aligned buttons evenly

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,17 +2,19 @@ var React = require('react');
 
 var Header = React.createClass({
 
-  placeholder: {search: "@handle, #hash, etc...", number: "Hours"},
+  placeholder: {search: "@handle, #hash, etc...", hours: "Hours", days: "Days"},
 
   handleSubmit: function(e) {
     e.preventDefault();
     var query = this.refs.query.value.trim();
-    var hours = this.refs.hours.value;
+    var hours = this.refs.hours.value.trim();
+    var days = this.refs.days.value.trim();
     if (!query)
       return;
-    this.props.onQuerySubmit({q: query, hours: hours});
+    this.props.onQuerySubmit({q: query, hours: hours, days: days});
     this.refs.query.value = "";
-    this.refs.hours.value = this.placeholder.number;
+    this.refs.hours.value = this.placeholder.hours;
+    this.refs.days.value = this.placeholder.days;
   },
 
   render: function() {
@@ -20,7 +22,8 @@ var Header = React.createClass({
       <div className={"header"}>
         <form onSubmit={this.handleSubmit}>
           <input type="search" placeholder={this.placeholder.search} ref="query" />
-          <input type="number" min="1" max="24" ref="hours" placeholder={this.placeholder.number}/>
+          <input type="number" min="0" max="24" ref="hours" placeholder={this.placeholder.hours}/>
+          <input type="number" min="0" max="30" ref="days" placeholder={this.placeholder.days}/>
           <input type="submit" />
         </form>
       </div>
