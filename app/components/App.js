12c463f337b6f15f41f7e414110d83020813bfaf
jhenderiks <justin@henderiks.ca>
2015-11-12 15:35:34 -0500

    Chart updating... need to set polling interval & handle resubmit case

--- app/components/App.js
+++ app/components/App.js
@@ -47,7 +47,26 @@ var App = React.createClass({
    */
   updateData: function(data, q) {
     // This is a repeat query
+    
+    data.time_labels = data.time_labels.map(function(label) {
+      return label.split(':').slice(0,2).join(':');
+    });
+
     if (this.state.q === q) {
+
+      var oldLastTimestamp = this.state.chartData.time_labels[this.state.chartData.time_labels.length - 1]
+      var indexOfFirstNewTimestamp = data.time_labels.indexOf(oldLastTimestamp) + 1;
+      
+      var newTimestamps = data.time_labels.slice(indexOfFirstNewTimestamp);
+      var newNeutralStats = data.stats.neutral.slice(indexOfFirstNewTimestamp);
+      var newNegativeStats = data.stats.negative.slice(indexOfFirstNewTimestamp);
+      var newPositiveStats = data.stats.positive.slice(indexOfFirstNewTimestamp);
+
+      data.time_labels = this.state.chartData.time_labels.slice(newTimestamps.length).concat(newTimestamps);
+      data.stats.neutral = this.state.chartData.stats.neutral.slice(newNeutralStats.length).concat(newNeutralStats);
+      data.stats.negative = this.state.chartData.stats.negative.slice(newNegativeStats.length).concat(newNegativeStats);
+      data.stats.positive = this.state.chartData.stats.positive.slice(newPositiveStats.length).concat(newPositiveStats);
+
       this.setState({
         mapData: {new: data.map, old: (this.state.mapData.new).concat(this.state.mapData.old)},
         tweetData: (this.state.tweetData).concat(data.tweets),
