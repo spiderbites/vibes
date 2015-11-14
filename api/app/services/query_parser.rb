a770b5836b37113354870d2120acb48d80bbedf4
Ajwah <johnson786.kevin+git@gmail.com>
2015-11-14 16:36:00 -0500

    Implement TimeParser skeleton that leverages Timestamp

--- api/app/services/query_parser.rb
+++ api/app/services/query_parser.rb
@@ -1,3 +1,39 @@
+class TimeParser
+  include Timestamp
+
+  def initialize(parameters)
+    @unit = obtain_time_unit(parameters)
+    @since = nil
+    @quantity = parameters[@unit]
+    @time_format = time_stamp(parameters, @since)
+  end
+
+  private
+    def obtain_time_unit(parameters)
+      (parameters.keys & TIMES)[0]
+    end
+
+    def time_stamp(parameters, since)
+      invoke(convert_time_to_method(parameters))
+    end
+
+    def invoke(time_method, since)
+      if since
+        method(time_method[:method]).call(time_method[:arg], since)
+      else
+        method(time_method[:method]).call(time_method[:arg])
+      end
+    end
+
+    def convert_time_to_method(parameters)
+      time = (parameters.keys & TIMES)[0]
+      {
+        method: ('past_' + time.to_s).to_sym,
+        arg: parameters[time].to_i
+      }
+    end
+end
+
 class QueryParser
 
   def initialize(parameters)
